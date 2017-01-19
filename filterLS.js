78701a6f2a71acff113e82b751e9d716be2fe230
Raphael Barbo <rsbarbo@gmail.com>
2017-01-19 14:23:35 -0700

    include ; properly

--- filterLS.js
+++ filterLS.js
@@ -1,8 +1,8 @@
 var fs = require('fs');
-var path = require('path')
+var path = require('path');
 
-var file = process.argv[2]
-var extension = process.argv[3]
+var file = process.argv[2];
+var extension = process.argv[3];
 
 fs.readdir(file, function(err, list){
   list.forEach(function(file){
