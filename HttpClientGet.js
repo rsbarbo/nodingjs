c6f34aeb578f654b1ea7f4ecdfa80573783ee897
Raphael Barbo <rsbarbo@gmail.com>
2017-01-19 15:55:37 -0700

    resolve HttpClientGet

--- HttpClientGet.js
+++ HttpClientGet.js
@@ -1,6 +1,8 @@
-var client = 
+var http = require('http');
 var url = process.argv[2]
 
 http.get(url, function(response){
-  console.log(response)
-})
+  response.on('data', function(data) {
+    console.log(data.toString());
+  });
+});
