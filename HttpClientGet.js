bfe4d465f7b1dddd9fb841aa2ff5a870b6aa54c9
Raphael Barbo <rsbarbo@gmail.com>
2017-01-24 18:16:38 -0700

    update master to match local copy

--- HttpClientGet.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-var url = process.argv[2]
-
-http.get(url, function(response){
-  response.on('data', function(data) {
-    console.log(data.toString());
-  });
-});
--- myFirstIO.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs')
-
-var str = fs.readFileSync(process.argv[2]).
-          toString();
-
-var outcome = str.split("\n").length - 1
-
-console.log(outcome);
